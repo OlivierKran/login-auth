7612eadaed536ac7977d3baa1aa2ed0d76c41cb4
Olivier <kleinolivier@orange.fr>
2023-07-31 09:11:59 +0200

    :construction: ajout d'une page 404 et style

---
+++ src/Pages/NotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './notfound.module.css';
+
+export default function NotFound(props) {
+    return(
+        <div className={styles.container}>
+            <h1> Page not found</h1>
+        </div>
+    );
+}
