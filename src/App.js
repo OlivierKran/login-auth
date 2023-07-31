763decdda46ef905219fddb2959a9c69ebabda7d
Olivier <kleinolivier@orange.fr>
2023-07-31 11:34:35 +0200

    :construction: index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './Context/context.js';
 import routes from './Config/routes.js';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {routes.map((route) =>(
-          <Route key={route.path} path={route.path} element={route.element} />
-        ))}
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
